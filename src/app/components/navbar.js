048f75e0bb088af532e23e38aceea12f04877f64
Abhishek Pandit <your_new_email@example.com>
2025-09-14 13:27:05 -0700

    reverted back'

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -1,18 +1,14 @@
 'use client'; 
 import "./navbar.css";
-import { useState } from "react";
 
 import Link from 'next/link';
 
 const sections = ["home", "projects",'resume'];
 
 export default function Navbar() {
-    const [active, setActive] = useState("home");
     const handleScroll = (id) => {
-    setActive(id);
     const el = document.getElementById(id);
     if (el) el.scrollIntoView({ behavior: 'smooth' });
-
   };
 
   return (
@@ -20,18 +16,15 @@ export default function Navbar() {
     <nav className="fly">
       <ul className="name" >
         {sections.map((sec) => (
-          
-          
           <li key={sec}>
             <button
                 type="button"
-                className={`bar ${active === sec ? "active" : ""}`}
+                className="bar"
                 onClick={() => handleScroll(sec)}
               >
                 {sec.charAt(0).toUpperCase() + sec.slice(1)}
               </button>
           </li>
-         
         ))}
       </ul>
     </nav>
